8affbd8e3741dc3868643ee27fd20984b38ed899
Bence Farkas <bence.farkas2003@gmail.com>
2025-04-12 15:57:57 +0200

    Create basic register UI

--- Bowling/src/app/register/register.component.ts
+++ Bowling/src/app/register/register.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-
+  registerForm: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
 }
