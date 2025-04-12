2bea5693e2f55aa84507ee29221480cfcc5eb2ab
Bence Farkas <bence.farkas2003@gmail.com>
2025-04-12 18:35:47 +0200

    Fetch alley data on app load

--- Bowling/src/app/app.component.ts
+++ Bowling/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
@@ -6,6 +6,8 @@ import { AboutComponent } from './about/about.component';
 import { BookComponent } from './book/book.component';
 import { UserService } from './user.service';
 import { CommonModule } from '@angular/common';
+import { AlleyService } from './alley.service';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -22,6 +24,11 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Bowling';
+  constructor(private alleyService: AlleyService) {}
+
+  ngOnInit(): void {
+    this.alleyService.fetchAlleyData();
+  }
 }
