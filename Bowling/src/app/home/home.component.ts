78756abf6e9e7b084a6e0a251f81e5ea1c6db7e6
Bence Farkas <bence.farkas2003@gmail.com>
2025-04-12 20:16:02 +0200

    Add cards to home page with alley data

--- Bowling/src/app/home/home.component.ts
+++ Bowling/src/app/home/home.component.ts
@@ -1,10 +1,20 @@
 import { Component, Input } from '@angular/core';
+import { AlleyService } from '../alley.service';
+import { Alley } from '../DTOs/alley';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  alleys: Alley[] = [];
+
+  constructor(private alleyService: AlleyService) {
+    this.alleys = this.alleyService.alleys;
+  }
+}
