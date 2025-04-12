fd0fd49f063698a9f3e02ed313062bb2ec646a35
Bence Farkas <bence.farkas2003@gmail.com>
2025-04-12 11:14:57 +0200

    Creat necessary components and write routing
    Also added angular material to the project

--- Bowling/src/app/app.routes.ts
+++ Bowling/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { BookComponent } from './book/book.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'book', component: BookComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: HomeComponent },
+];
