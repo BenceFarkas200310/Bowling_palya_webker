f0782ce90aafdb3f80508dd31f7734f0631426e9
Bence Farkas <bence.farkas2003@gmail.com>
2025-04-12 12:03:02 +0200

    Implement routing

--- Bowling/src/app/app.component.ts
+++ Bowling/src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BookComponent } from './book/book.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    BookComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Bowling';
